b55d04d2733e01f71f3f6df1db3f0b4a63931fe3
VTI\duc.phamvan <kimconoc@gmail.com>
2023-06-02 15:57:57 +0700

    update code

--- ToolReadExcel/Form1.Designer.cs
+++ ToolReadExcel/Form1.Designer.cs
@@ -28,80 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnSubmit = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pathExcel = new System.Windows.Forms.TextBox();
             this.strConnect = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.tetNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtCmdcd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtMdkikaku = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtUpdt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnSubmit.Location = new System.Drawing.Point(351, 123);
+            resources.ApplyResources(this.btnSubmit, "btnSubmit");
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(96, 33);
-            this.btnSubmit.TabIndex = 0;
-            this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
             this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 51);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Excel path";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 81);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "String connect";
             // 
             // pathExcel
             // 
-            this.pathExcel.Location = new System.Drawing.Point(123, 51);
+            resources.ApplyResources(this.pathExcel, "pathExcel");
             this.pathExcel.Name = "pathExcel";
-            this.pathExcel.Size = new System.Drawing.Size(647, 23);
-            this.pathExcel.TabIndex = 3;
             // 
             // strConnect
             // 
-            this.strConnect.Location = new System.Drawing.Point(123, 81);
+            resources.ApplyResources(this.strConnect, "strConnect");
             this.strConnect.Name = "strConnect";
-            this.strConnect.Size = new System.Drawing.Size(647, 23);
-            this.strConnect.TabIndex = 4;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(336, 9);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 32);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Update DB";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tetNo,
+            this.txtCmdcd,
+            this.txtMdkikaku,
+            this.txtUpdt});
+            resources.ApplyResources(this.dataGridView, "dataGridView");
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowTemplate.Height = 25;
+            // 
+            // tetNo
+            // 
+            resources.ApplyResources(this.tetNo, "tetNo");
+            this.tetNo.Name = "tetNo";
+            this.tetNo.ReadOnly = true;
+            // 
+            // txtCmdcd
+            // 
+            resources.ApplyResources(this.txtCmdcd, "txtCmdcd");
+            this.txtCmdcd.Name = "txtCmdcd";
+            this.txtCmdcd.ReadOnly = true;
+            // 
+            // txtMdkikaku
+            // 
+            resources.ApplyResources(this.txtMdkikaku, "txtMdkikaku");
+            this.txtMdkikaku.Name = "txtMdkikaku";
+            this.txtMdkikaku.ReadOnly = true;
+            // 
+            // txtUpdt
+            // 
+            resources.ApplyResources(this.txtUpdt, "txtUpdt");
+            this.txtUpdt.Name = "txtUpdt";
+            this.txtUpdt.ReadOnly = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 178);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.strConnect);
             this.Controls.Add(this.pathExcel);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSubmit);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Update DB";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,5 +140,10 @@
         private TextBox pathExcel;
         private TextBox strConnect;
         private Label label3;
+        private DataGridView dataGridView;
+        private DataGridViewTextBoxColumn tetNo;
+        private DataGridViewTextBoxColumn txtCmdcd;
+        private DataGridViewTextBoxColumn txtMdkikaku;
+        private DataGridViewTextBoxColumn txtUpdt;
     }
 }
--- ToolReadExcel/Form1.cs
+++ ToolReadExcel/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using OfficeOpenXml;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
 
 namespace ToolReadExcel
 {
@@ -9,10 +11,16 @@ namespace ToolReadExcel
         public Form1()
         {
             InitializeComponent();
+            dataGridView.ColumnHeadersDefaultCellStyle.BackColor = Color.MediumAquamarine;
+            dataGridView.EnableHeadersVisualStyles = false;
+            dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            dataGridView.AllowUserToResizeRows = false;
+            dataGridView.AllowUserToResizeColumns = false;
         }
-        DataSet ds;
         private void button1_Click(object sender, EventArgs e)
         {
+            btnSubmit.Enabled= false;
             if (string.IsNullOrEmpty(pathExcel.Text) || string.IsNullOrEmpty(strConnect.Text))
             {
                 MessageBox.Show("Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -20,52 +28,66 @@ namespace ToolReadExcel
             }
             try
             {
+                string connectionString = strConnect.Text;
                 string filePath = pathExcel.Text;
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+                // Tạo đối tượng SqlConnection
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    // Lấy Sheet đầu tiên từ file Excel
-                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-
-                    // Lấy số lượng hàng và cột trong Sheet
-                    int rowCount = worksheet.Dimension.Rows;
-                    int colCount = worksheet.Dimension.Columns;
-
-                    // Đọc dữ liệu từ từng ô trong Sheet
-                    for (int row = 2; row <= rowCount; row++)
+                    // Mở kết nối
+                    connection.Open();
+                    // ReadFileExcel
+                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                    using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
                     {
-                        // Lấy giá trị từ ô tại vị trí (row, col)
-                        var cellValueCMDCD = worksheet.Cells[row, 1].Value;
-                        var cellValue = worksheet.Cells[row, 10].Value;
-                        // Xử lý giá trị từ ô tại vị trí (row, col)
+                        // Lấy Sheet đầu tiên từ file Excel
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                        // Lấy số lượng hàng và cột trong Sheet
+                        int rowCount = worksheet.Dimension.Rows;
+                        int colCount = worksheet.Dimension.Columns;
+
+                        // Đọc dữ liệu từ từng ô trong Sheet
+                        for (int row = 2; row <= rowCount; row++)
+                        {
+                            // Lấy giá trị từ ô tại vị trí (row, col)
+                            var cellValueCMDCD = worksheet.Cells[row, 1].Value?.ToString();
+                            var cellValueMdkikaku = worksheet.Cells[row, 10].Value?.ToString();
+                            // Đoạn mã SQL UPDATE
+                            string sqlUpdate = string.Format("update LSYOHIN_MST set UP_DT = GETDATE(), MDKIKAKU = '{0}' where CMDCD = '{1}'", cellValueMdkikaku, cellValueCMDCD);
+                            // Tạo đối tượng SqlCommand với câu lệnh SQL và kết nối
+                            using (SqlCommand command = new SqlCommand(sqlUpdate, connection))
+                            {
+                                // Thực thi câu lệnh UPDATE
+                                command.ExecuteNonQuery();
+                            }
+                            ShowDataGridView(cellValueCMDCD, cellValueMdkikaku);
+                        }
                     }
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnSubmit.Enabled = true;
                 return;
             }
           
-
-            //string connectionString = strConnect.Text;
-
-            //// Đoạn mã SQL UPDATE
-            //string sqlUpdate = "UPDATE TableName SET Column1 = 'NewValue1', Column2 = 'NewValue2' WHERE Condition";
-
-            //// Tạo đối tượng SqlConnection
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    // Mở kết nối
-            //    connection.Open();
-
-            //    // Tạo đối tượng SqlCommand với câu lệnh SQL và kết nối
-            //    using (SqlCommand command = new SqlCommand(sqlUpdate, connection))
-            //    {
-            //        // Thực thi câu lệnh UPDATE
-            //        command.ExecuteNonQuery();
-            //    }
-            //}
+        }
+        int _count = 0;
+        private void ShowDataGridView(string? cellValueCMDCD,string? cellValueMdkikaku)
+        {
+            dataGridView.Invoke(new MethodInvoker(delegate ()
+            {
+                var row = new DataGridView();
+                dataGridView.Rows.Add(row);
+                dataGridView.Rows[_count].Cells[0].Value = _count + 1;
+                dataGridView.Rows[_count].Cells[1].Value = cellValueCMDCD;
+                dataGridView.Rows[_count].Cells[2].Value = cellValueMdkikaku;
+                dataGridView.Rows[_count].Cells[3].Value = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt");
+                _count++;
+            }));
+            //Điều chỉnh thanh Scroll theo vị trí mong muốn
+            dataGridView.FirstDisplayedScrollingRowIndex = dataGridView.RowCount - 1;
+            Task.Delay(100).Wait();
         }
     }
 }
