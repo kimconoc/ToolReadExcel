9acfefd39a09dfbd8e95ba2f76fb63416f6828f6
VTI\duc.phamvan <kimconoc@gmail.com>
2023-06-02 09:07:53 +0700

    push code

---
+++ ToolReadExcel/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ToolReadExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pathExcel = new System.Windows.Forms.TextBox();
+            this.strConnect = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSubmit.Location = new System.Drawing.Point(351, 123);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(96, 33);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Excel path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "String connect";
+            // 
+            // pathExcel
+            // 
+            this.pathExcel.Location = new System.Drawing.Point(123, 51);
+            this.pathExcel.Name = "pathExcel";
+            this.pathExcel.Size = new System.Drawing.Size(647, 23);
+            this.pathExcel.TabIndex = 3;
+            // 
+            // strConnect
+            // 
+            this.strConnect.Location = new System.Drawing.Point(123, 81);
+            this.strConnect.Name = "strConnect";
+            this.strConnect.Size = new System.Drawing.Size(647, 23);
+            this.strConnect.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(336, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Update DB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 178);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.strConnect);
+            this.Controls.Add(this.pathExcel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "Update DB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnSubmit;
+        private Label label1;
+        private Label label2;
+        private TextBox pathExcel;
+        private TextBox strConnect;
+        private Label label3;
+    }
+}
---
+++ ToolReadExcel/Form1.cs
@@ -0,0 +1,73 @@
+﻿using OfficeOpenXml;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ToolReadExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataSet ds;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(pathExcel.Text) || string.IsNullOrEmpty(strConnect.Text))
+            {
+                MessageBox.Show("Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                string filePath = pathExcel.Text;
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    // Lấy Sheet đầu tiên từ file Excel
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                    // Lấy số lượng hàng và cột trong Sheet
+                    int rowCount = worksheet.Dimension.Rows;
+                    int colCount = worksheet.Dimension.Columns;
+
+                    // Đọc dữ liệu từ từng ô trong Sheet
+                    for (int row = 1; row <= rowCount; row++)
+                    {
+                        for (int col = 1; col <= colCount; col++)
+                        {
+                            // Lấy giá trị từ ô tại vị trí (row, col)
+                            object cellValue = worksheet.Cells[row, col].Value;
+                            // Xử lý giá trị từ ô tại vị trí (row, col)
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+          
+
+            //string connectionString = strConnect.Text;
+
+            //// Đoạn mã SQL UPDATE
+            //string sqlUpdate = "UPDATE TableName SET Column1 = 'NewValue1', Column2 = 'NewValue2' WHERE Condition";
+
+            //// Tạo đối tượng SqlConnection
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    // Mở kết nối
+            //    connection.Open();
+
+            //    // Tạo đối tượng SqlCommand với câu lệnh SQL và kết nối
+            //    using (SqlCommand command = new SqlCommand(sqlUpdate, connection))
+            //    {
+            //        // Thực thi câu lệnh UPDATE
+            //        command.ExecuteNonQuery();
+            //    }
+            //}
+        }
+    }
+}
